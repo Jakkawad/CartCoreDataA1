ba6f7c6a8e78f57a6de18441c996fc545d1d20d4
Jakkawad <sunnysunoc@gmail.com>
2016-08-27 19:48:15 +0700

    Layout

---
+++ CartCoreDataA1/PageOptionViewController.swift
@@ -0,0 +1,57 @@
+//
+//  PageOptionViewController.swift
+//  CartCoreDataA1
+//
+//  Created by Jakkawad Chaiplee on 8/27/2559 BE.
+//  Copyright © 2559 Jakkawad Chaiplee. All rights reserved.
+//
+
+import UIKit
+
+class PageOptionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView:UITableView!
+    
+    @IBAction func btnAdd(sender: AnyObject) {
+        print("Add")
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell0 = tableView.dequeueReusableCellWithIdentifier("tableCell0")
+        
+        return cell0!
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
